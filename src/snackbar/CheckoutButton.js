2e0d3db78588ac10ef0fcf1919737e3c079ac568
Leandro Angeli <leandroandresangeli@gmail.com>
2022-06-27 17:05:17 -0300

    fixed little issue in button

--- src/snackbar/CheckoutButton.js
+++ src/snackbar/CheckoutButton.js
@@ -3,7 +3,12 @@ import React from 'react';
 
 const CheckoutButton = () => {
 	return (
-		<Button variant="outlined" color="secondary" size="small">
+		<Button
+			variant="outlined"
+			color="secondary"
+			size="small"
+			sx={{ textDecoration: 'none' }}
+		>
 			go to checkout
 		</Button>
 	);
